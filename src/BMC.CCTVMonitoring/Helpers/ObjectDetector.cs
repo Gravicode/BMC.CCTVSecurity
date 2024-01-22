261a35787fbe089658f77f635d1b060920262a2d
mifmasterz <mifmasterz@yahoo.com>
2024-01-22 07:50:57 +0700

    change to v8

--- src/BMC.CCTVMonitoring/Helpers/ObjectDetector.cs
+++ src/BMC.CCTVMonitoring/Helpers/ObjectDetector.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Yolov7net;
+//using Yolov8Net;
 
 namespace BMC.CCTVMonitoring.Helpers
 {
@@ -25,7 +26,7 @@ namespace BMC.CCTVMonitoring.Helpers
         ConcurrentQueue<DetectedObject> ListDetections = new ConcurrentQueue<DetectedObject>();
         public EventHandler<DetectedObject> OnDetectedObject;
         bool IsReady { set; get; }
-        Yolov7 yolo { set; get; }
+        Yolov8 yolo { set; get; }
         public ObjectDetector()
         {
             Setup();
@@ -43,7 +44,8 @@ namespace BMC.CCTVMonitoring.Helpers
             try
             {
                 // init Yolov8 with onnx (include nms results)file path
-                yolo = new Yolov7("./Assets/yolov7-tiny_640x640.onnx", false);
+                //yolo = new Yolov7("./Assets/yolov7-tiny_640x640.onnx", false);
+                yolo = new Yolov8("./Assets/yolov8n.onnx", false);
                 
                 // setup labels of onnx model 
                 yolo.SetupYoloDefaultLabels();   // use custom trained model should use your labels like: yolo.SetupLabels(string[] labels)
@@ -75,6 +77,16 @@ namespace BMC.CCTVMonitoring.Helpers
             {
                 predictions.Remove(item);
             }
+            using var graphics = Graphics.FromImage(image);
+            foreach (var prediction in predictions) // iterate predictions to draw results
+            {
+                double score = Math.Round(prediction.Score, 2);
+                graphics.DrawRectangles(new Pen(prediction.Label.Color, 1), new[] { prediction.Rectangle });
+                var (x, y) = (prediction.Rectangle.X - 3, prediction.Rectangle.Y - 23);
+                graphics.DrawString($"{prediction.Label.Name} ({score})",
+                                new Font("Consolas", 16, GraphicsUnit.Pixel), new SolidBrush(prediction.Label.Color),
+                                new PointF(x, y));
+            }
             /*
             using var graphics = Graphics.FromImage(image);
             foreach (var prediction in predictions) // iterate predictions to draw results
@@ -87,6 +99,7 @@ namespace BMC.CCTVMonitoring.Helpers
                                 new PointF(x, y));
             }
             */
+            /*
             var desc = string.Empty;
             using (Graphics graphics = Graphics.FromImage(image))
             {
@@ -130,7 +143,7 @@ namespace BMC.CCTVMonitoring.Helpers
                     graphics.DrawRectangle(pen, x, y, width, height);
                 }
             }
-             
+            */
             var newObj = new DetectedObject() { DetectedTime = DateTime.Now, No = No, Predictions = predictions, AnotatedImage = image };
             ListDetections.Enqueue(newObj);
             //keep the max items = 100
